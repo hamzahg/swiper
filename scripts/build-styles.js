8c1da648f52aed8182f00703f9c25f030bc2c0db
Hamzah Ghafoor <hamzah@matterofform.com>
2020-02-13 11:42:11 +0000

    added dist folder for vue-awesome-swiper compatibilty

--- scripts/build-styles.js
+++ scripts/build-styles.js
@@ -1,29 +1,29 @@
 /* eslint import/no-extraneous-dependencies: ["error", {"devDependencies": true}] */
 /* eslint no-console: "off" */
 
-const fs = require('fs');
-const path = require('path');
-const glob = require('glob');
-const fse = require('./utils/fs-extra.js');
-const less = require('./utils/less.js');
-const autoprefixer = require('./utils/autoprefixer.js');
-const cleanCSS = require('./utils/clean-css.js');
-const banner = require('./banner.js');
+const fs = require("fs");
+const path = require("path");
+const glob = require("glob");
+const fse = require("./utils/fs-extra.js");
+const less = require("./utils/less.js");
+const autoprefixer = require("./utils/autoprefixer.js");
+const cleanCSS = require("./utils/clean-css.js");
+const banner = require("./banner.js");
 
-const config = require('./build-config.js');
+const config = require("./build-config.js");
 
 function base64Encode(file) {
   // read binary data
   const bitmap = fs.readFileSync(file);
   // convert binary data to base64 encoded string
-  return Buffer.from(bitmap).toString('base64');
+  return Buffer.from(bitmap).toString("base64");
 }
 
 async function build(cb) {
-  const env = process.env.NODE_ENV || 'development';
+  const env = process.env.NODE_ENV || "development";
 
   const components = [];
-  config.components.forEach((name) => {
+  config.components.forEach(name => {
     const lessFilePath = `./src/components/${name}/${name}.less`;
 
     if (fs.existsSync(lessFilePath)) {
@@ -33,50 +33,66 @@ async function build(cb) {
 
   const colors = [];
 
-  Object.keys(config.colors).forEach((key) => {
+  Object.keys(config.colors).forEach(key => {
     colors.push(`${key} ${config.colors[key]}`);
   });
 
-  let lessContent = fs.readFileSync(path.resolve(__dirname, '../src/swiper.less'), 'utf8');
+  let lessContent = fs.readFileSync(
+    path.resolve(__dirname, "../src/swiper.less"),
+    "utf8"
+  );
   lessContent = lessContent
-    .replace('//IMPORT_COMPONENTS', components.map((component) => `@import url('./components/${component}/${component}.less');`).join('\n'))
-    .replace('$themeColor', config.themeColor)
-    .replace('$colors', colors.join(', '));
+    .replace(
+      "//IMPORT_COMPONENTS",
+      components
+        .map(
+          component =>
+            `@import url('./components/${component}/${component}.less');`
+        )
+        .join("\n")
+    )
+    .replace("$themeColor", config.themeColor)
+    .replace("$colors", colors.join(", "));
 
   let cssContent;
   try {
     cssContent = await autoprefixer(
-      await less(lessContent, path.resolve(__dirname, '../src'))
+      await less(lessContent, path.resolve(__dirname, "../src"))
     );
   } catch (err) {
     console.log(err);
   }
 
   // Write file
-  fse.writeFileSync(`./${env === 'development' ? 'build' : 'package'}/css/swiper.css`, `${banner}\n${cssContent}`);
+  fse.writeFileSync(
+    `./${env === "development" ? "build" : "dist"}/css/swiper.css`,
+    `${banner}\n${cssContent}`
+  );
 
-  if (env === 'development') {
+  if (env === "development") {
     if (cb) cb();
     return;
   }
 
   // Copy less & scss
-  const iconsFontBase64 = base64Encode('./src/icons/font/swiper-icons.woff');
+  const iconsFontBase64 = base64Encode("./src/icons/font/swiper-icons.woff");
 
-  glob('**/*.*', { cwd: path.resolve(__dirname, '../src') }, (err, files) => {
+  glob("**/*.*", { cwd: path.resolve(__dirname, "../src") }, (err, files) => {
     files.forEach((file, index) => {
-      if (file.indexOf('icons/') === 0) return;
-      if (file.indexOf('.js') >= 0) return;
+      if (file.indexOf("icons/") === 0) return;
+      if (file.indexOf(".js") >= 0) return;
 
-      let fileContent = fse.readFileSync(path.resolve(__dirname, '../src', file));
-      if (file.indexOf('swiper.less') >= 0) {
+      let fileContent = fse.readFileSync(
+        path.resolve(__dirname, "../src", file)
+      );
+      if (file.indexOf("swiper.less") >= 0) {
         fileContent = fileContent
-          .replace('swiperIconsFont()', `'${iconsFontBase64}'`)
-          .replace('$themeColor', config.themeColor)
-          .replace('$colors', colors.join(', '));
+          .replace("swiperIconsFont()", `'${iconsFontBase64}'`)
+          .replace("$themeColor", config.themeColor)
+          .replace("$colors", colors.join(", "));
       }
 
-      fse.writeFileSync(path.resolve(__dirname, '../package', file), fileContent);
+      fse.writeFileSync(path.resolve(__dirname, "../dist", file), fileContent);
       if (index === files.length - 1) cb();
     });
   });
@@ -85,7 +101,10 @@ async function build(cb) {
   const minifiedContent = await cleanCSS(cssContent);
 
   // Write file
-  fse.writeFileSync('./package/css/swiper.min.css', `${banner}\n${minifiedContent}`);
+  fse.writeFileSync(
+    "./dist/css/swiper.min.css",
+    `${banner}\n${minifiedContent}`
+  );
 
   if (cb) cb();
 }
--- scripts/gulpfile.js
+++ scripts/gulpfile.js
@@ -1,58 +1,63 @@
 /* eslint no-console: ["error", { allow: ["log"] }] */
-const gulp = require('gulp');
-const connect = require('gulp-connect');
-const gopen = require('gulp-open');
-const fs = require('fs');
-const path = require('path');
+const gulp = require("gulp");
+const connect = require("gulp-connect");
+const gopen = require("gulp-open");
+const fs = require("fs");
+const path = require("path");
 
-const buildJs = require('./build-js.js');
-const buildStyles = require('./build-styles.js');
+const buildJs = require("./build-js.js");
+const buildStyles = require("./build-styles.js");
 
 // Tasks
-gulp.task('playground', (cb) => {
-  const env = process.env.NODE_ENV || 'development';
-  let content = fs.readFileSync(path.resolve(__dirname, '../playground/index.html'), 'utf8');
-  if (env === 'development') {
+gulp.task("playground", cb => {
+  const env = process.env.NODE_ENV || "development";
+  let content = fs.readFileSync(
+    path.resolve(__dirname, "../playground/index.html"),
+    "utf8"
+  );
+  if (env === "development") {
     content = content
-      .replace('../package/css/swiper.min.css', '../build/css/swiper.css')
-      .replace('../package/js/swiper.min.js', '../build/js/swiper.js');
+      .replace("../dist/css/swiper.min.css", "../build/css/swiper.css")
+      .replace("../dist/js/swiper.min.js", "../build/js/swiper.js");
   } else {
     content = content
-      .replace('../build/css/swiper.css', '../package/css/swiper.min.css')
-      .replace('../build/js/swiper.js', '../package/js/swiper.min.js');
+      .replace("../build/css/swiper.css", "../dist/css/swiper.min.css")
+      .replace("../build/js/swiper.js", "../dist/js/swiper.min.js");
   }
-  fs.writeFileSync(path.resolve(__dirname, '../playground/index.html'), content);
+  fs.writeFileSync(
+    path.resolve(__dirname, "../playground/index.html"),
+    content
+  );
   if (cb) cb();
 });
-gulp.task('js', (cb) => {
+gulp.task("js", cb => {
   buildJs(cb);
 });
 
-gulp.task('styles', (cb) => {
+gulp.task("styles", cb => {
   buildStyles(cb);
 });
 
-
-// in prod builds, copy /src folder into /package
-gulp.task('prod-source-copy', (cb) => {
-  const env = process.env.NODE_ENV || 'development';
-  if (env === 'production') {
-    gulp.src(['./src/**/*']).pipe(gulp.dest('./package/src/'));
+// in prod builds, copy /src folder into /dist
+gulp.task("prod-source-copy", cb => {
+  const env = process.env.NODE_ENV || "development";
+  if (env === "production") {
+    gulp.src(["./src/**/*"]).pipe(gulp.dest("./dist/src/"));
   }
   if (cb) cb();
 });
 
 // in prod builds, adjust sourcemap paths to actual src location
-gulp.task('prod-source-sourcemap-fix-paths', (cb) => {
-  const env = process.env.NODE_ENV || 'development';
-  if (env === 'production') {
-    const jsDir = path.resolve(__dirname, '../package/js/');
+gulp.task("prod-source-sourcemap-fix-paths", cb => {
+  const env = process.env.NODE_ENV || "development";
+  if (env === "production") {
+    const jsDir = path.resolve(__dirname, "../dist/js/");
     const mapFiles = fs
       .readdirSync(jsDir)
-      .filter((file) => file.toLowerCase().endsWith('.map'));
-    mapFiles.forEach((mapFile) => {
+      .filter(file => file.toLowerCase().endsWith(".map"));
+    mapFiles.forEach(mapFile => {
       const mapFilePath = path.resolve(jsDir, mapFile);
-      let content = fs.readFileSync(mapFilePath, 'utf8');
+      let content = fs.readFileSync(mapFilePath, "utf8");
       content = content
         .replace(/"\.\.\/\.\.\//g, '"../')
         .replace(/"\.\.\/node_modules\//g, '"~/');
@@ -62,25 +67,35 @@ gulp.task('prod-source-sourcemap-fix-paths', (cb) => {
   if (cb) cb();
 });
 
-gulp.task('build', gulp.series(['js', 'styles', 'prod-source-copy', 'prod-source-sourcemap-fix-paths']));
+gulp.task(
+  "build",
+  gulp.series([
+    "js",
+    "styles",
+    "prod-source-copy",
+    "prod-source-sourcemap-fix-paths"
+  ])
+);
 
-gulp.task('watch', () => {
-  gulp.watch('./src/**/**/*.js', gulp.series('js'));
-  gulp.watch('./src/**/**/*.less', gulp.series('styles'));
+gulp.task("watch", () => {
+  gulp.watch("./src/**/**/*.js", gulp.series("js"));
+  gulp.watch("./src/**/**/*.less", gulp.series("styles"));
 });
 
-gulp.task('connect', () => {
+gulp.task("connect", () => {
   connect.server({
-    root: ['./'],
+    root: ["./"],
     livereload: true,
-    port: '3000',
+    port: "3000"
   });
 });
 
-gulp.task('open', () => {
-  gulp.src('./playground/index.html').pipe(gopen({ uri: 'http://localhost:3000/playground/' }));
+gulp.task("open", () => {
+  gulp
+    .src("./playground/index.html")
+    .pipe(gopen({ uri: "http://localhost:3000/playground/" }));
 });
 
-gulp.task('server', gulp.parallel(['watch', 'connect', 'open']));
+gulp.task("server", gulp.parallel(["watch", "connect", "open"]));
 
-gulp.task('default', gulp.series('server'));
+gulp.task("default", gulp.series("server"));
